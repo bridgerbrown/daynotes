9b1c74f1facafdbce013d6a9b673724eb5217f80
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-03-14 15:31:27 -0700

    draft of calendar done

--- components/calendar/calendar.tsx
+++ components/calendar/calendar.tsx
@@ -7,6 +7,8 @@ export default function Calendar(){
     let [currentYear, setCurrentYear] = useState<any>(date.getFullYear())
     let [currentMonth, setCurrentMonth] = useState<any>(date.getMonth())
 
+    const dayStyle: string = ``
+
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
     const renderCalendar = () => {
@@ -17,16 +19,16 @@ export default function Calendar(){
         let liTag = []
 
         for (let i = firstDayOfMonth; i > 0; i--) {
-            liTag.push(<li className="w-day inactive-day">{lastDateOfLastMonth - i + 1}</li>)
+            liTag.push(<li className="w-day inactive-day py-4">{lastDateOfLastMonth - i + 1}</li>)
         }
         
         for (let i = 1; i <= lastDateOfMonth; i++) {
-            let isToday = i === date.getDate() && currentMonth === new Date().getMonth() && currentYear === new Date().getFullYear() ? "active-day" : "";
-            liTag.push(<li className="w-day ${isToday}">{i}</li>)
+            let isToday = i === date.getDate() && currentMonth === new Date().getMonth() && currentYear === new Date().getFullYear() ? <li className="w-day bg-red-400 rounded-full py-4">{i}</li> : <li className="w-day py-4">{i}</li>;
+            liTag.push(isToday)
         }
 
         for (let i = lastDayOfMonth; i < 6; i++) {
-            liTag.push(<li className="w-day inactive-day">{i - lastDayOfMonth + 1}</li>)
+            liTag.push(<li className="w-day inactive-day py-4">{i - lastDayOfMonth + 1}</li>)
         }
         setCurrentDate(`${months[currentMonth]} ${currentYear}`)
         setDays(liTag)
