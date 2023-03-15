4ba486f966a5264187814432c184720679b7527d
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-03-15 15:28:22 -0700

    restyled weekly view to larger

--- components/calendar/weekly.tsx
+++ components/calendar/weekly.tsx
@@ -94,14 +94,14 @@ export default function Weekly() {
 
   console.log(week)
 
-  function previousMonth() {
-    let firstDayNextMonth = add(firstDayCurrentMonth, { months: -1 })
-    setCurrentMonth(format(firstDayNextMonth, 'MMM-yyyy'))
+  function previousWeek() {
+    let firstDayNextWeek = add(firstDayCurrentWeek, { weeks: -1 })
+    setCurrentWeek(format(firstDayNextWeek, 'w'))
   }
 
-  function nextMonth() {
-    let firstDayNextMonth = add(firstDayCurrentMonth, { months: 1 })
-    setCurrentMonth(format(firstDayNextMonth, 'MMM-yyyy'))
+  function nextWeek() {
+    let firstDayNextWeek = add(firstDayCurrentWeek, { weeks: 1 })
+    setCurrentWeek(format(firstDayNextWeek, 'w'))
   }
 
   let selectedDayMeetings = meetings.filter((meeting) =>
@@ -111,22 +111,22 @@ export default function Weekly() {
   return (
     <div className="pt-16">
       <div className="border-2 py-12 rounded-lg max-w-md px-4 mx-auto sm:px-7 md:max-w-6xl md:px-6">
-        <div className="md:grid md:grid-cols-2 md:divide-x md:divide-gray-200">
-          <div className="md:pr-14">
+        <div className="smd:grid md:divide-x md:divide-gray-200">
+          <div className="mb-16 md:pr-14">
             <div className="flex items-center">
-              <h2 className="flex-auto font-semibold text-gray-100">
-                {format(firstDayCurrentMonth, 'MMMM yyyy')}
+              <h2 className="flex-auto text-2xl font-semibold text-gray-100">
+                {format(selectedDay, 'MMMM d, yyyy')}
               </h2>
               <button
                 type="button"
-                onClick={previousMonth}
+                onClick={previousWeek}
                 className="-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-200 hover:text-gray-500"
               >
                 <span className="sr-only">Previous month</span>
                 <ChevronLeftIcon className="w-5 h-5" aria-hidden="true" />
               </button>
               <button
-                onClick={nextMonth}
+                onClick={nextWeek}
                 type="button"
                 className="-my-1.5 -mr-1.5 ml-2 flex flex-none items-center justify-center p-1.5 text-gray-200 hover:text-gray-500"
               >
@@ -134,7 +134,7 @@ export default function Weekly() {
                 <ChevronRightIcon className="w-5 h-5" aria-hidden="true" />
               </button>
             </div>
-            <div className="grid grid-cols-7 mt-10 text-xs leading-6 text-center text-gray-200">
+            <div className="grid grid-cols-7 mt-10 text-xl leading-6 text-center text-gray-200">
               <div>S</div>
               <div>M</div>
               <div>T</div>
@@ -143,7 +143,7 @@ export default function Weekly() {
               <div>F</div>
               <div>S</div>
             </div>
-            <div className="grid grid-cols-7 mt-2 text-sm">
+            <div className="grid grid-cols-7 mt-4 text-2xl">
               {week.map((day, dayIdx) => (
                 <div
                   key={day.toString()}
@@ -156,26 +156,26 @@ export default function Weekly() {
                     type="button"
                     onClick={() => setSelectedDay(day)}
                     className={classNames(
-                      isEqual(day, selectedDay) && 'text-white',
+                      isEqual(day, selectedDay) && 'text-slate-900 bg-white w-12 h-12',
                       !isEqual(day, selectedDay) &&
                         isToday(day) &&
-                        'text-red-500',
+                        'text-red-500 w-12 h-12',
                       !isEqual(day, selectedDay) &&
                         !isToday(day) &&
                         isSameMonth(day, firstDayCurrentWeek) &&
-                        'text-gray-200',
+                        'text-gray-200 w-12 h-12',
                       !isEqual(day, selectedDay) &&
                         !isToday(day) &&
                         !isSameMonth(day, firstDayCurrentWeek) &&
-                        'text-gray-400',
-                      isEqual(day, selectedDay) && isToday(day) && 'bg-red-500',
+                        'text-gray-400 w-12 h-12',
+                      isEqual(day, selectedDay) && isToday(day) && 'flex justify-center items-center text-center w-12 h-12 bg-red-500',
                       isEqual(day, selectedDay) &&
                         !isToday(day) &&
-                        'bg-gray-900',
-                      !isEqual(day, selectedDay) && 'hover:bg-gray-200',
+                        'bg-gray-900 w-12 h-12',
+                      !isEqual(day, selectedDay) && 'hover:bg-gray-200 hover:text-slate-900 w-12 h-12',
                       (isEqual(day, selectedDay) || isToday(day)) &&
-                        'font-semibold',
-                      'mx-auto flex h-8 w-8 items-center justify-center rounded-full'
+                        'font-semibold w-12 h-12',
+                      'mx-auto flex w-12 h-12 items-center justify-center rounded-full'
                     )}
                   >
                     <time dateTime={format(day, 'yyyy-MM-dd')}>
