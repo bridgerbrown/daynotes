716118f626c22ad83ad03c30e2e38b6770ec3c55
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-07-01 19:58:15 -0700

    little bugs

--- client/pages/[user]/notes.tsx
+++ client/pages/[user]/notes.tsx
@@ -29,6 +29,8 @@ export default function Notes({userCtxt}: InferGetServerSidePropsType<typeof get
     })
   }
 
+  console.log(usersNotes)
+
   async function getUserNotes(userId: string){
     await fetch(`/api/notes?userId=${userId}`)
       .then(response => response.json())
