89ed501c27e6d453bb6869cf81078915be01fc56
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-07-11 19:36:18 -0700

    meta, readme, fav

---
+++ README.md
@@ -0,0 +1,6 @@
+![DayNotes Preview](note-preview.png)
+
+DayNotes is a Front-End web development project that allows users to make date-based note documents using the Quill.js text editor alongside
+a calendar that visualizes your created notes. When a user signs up, their notes are stored within a MongoDB database behind
+their secure Auth0 user validation. Utilizing the power of Socket.io web sockets, DayNotes allows users to have 
+instantaneously updating documents for real-time synchronization between your browser tabs.
--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -4,6 +4,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta property="og:title" content="DayNotes"/>
+        <meta property="og:image" content={"/note-sample.png"}/>
+        <meta property="og:description" content="DayNotes date-based note taking platform"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, target-densityDpi=device-dpi"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
       </Head>
       <body>
         <Main />
