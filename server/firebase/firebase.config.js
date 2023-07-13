26c23ae0c9f75c1f31851de9c23414255cadafe6
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-07-12 17:36:43 -0700

    firebase functions, express, node added

---
+++ server/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
---
+++ server/functions/src/index.ts
@@ -0,0 +1,124 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import { MongoClient } from 'mongodb';
+import * as express from 'express';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
+
+admin.initializeApp();
+
+require('dotenv').config()
+
+const client = new MongoClient(process.env.MONGODB_CONNECTION_STRING!);
+
+let database: any;
+
+async function connectToDatabase() {
+  if (!database) {
+    await client.connect();
+    database = client.db('notes-db');
+  }
+  return database;
+}
+
+function isOnlyWhiteSpace(note: string){
+  return /^\s*$/.test(note);
+}
+
+const app = express();
+exports.api = functions.https.onRequest(app);
+
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+      origin: "https://daynotes-ebon.vercel.app/",
+      methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket: Socket) => {
+  socket.on("get-document", async (userId: string, date: string) => {
+    const documentId = `${userId}-${date}`;
+    const document = await findOrCreateDocument(documentId, userId, date);
+    socket.join(documentId);
+    socket.emit('load-document', document.data);
+
+  socket.on("send-changes", (delta: any) => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta)
+  });
+
+  socket.on("save-document", async (data: any) => {
+      await saveDocument(documentId, data);  
+  });
+
+  socket.on("delete-note", async () => {
+      await deleteDocument(documentId);
+  });
+
+  socket.on("disconnect", async () => {
+      const note = await findDocument(documentId);
+      if (note && note.data.ops) {
+        const noteData = note.data.ops[0].insert;
+        if (noteData.length == 1 || isOnlyWhiteSpace(noteData)){
+          await deleteDocument(documentId);
+        }
+      }
+    })
+
+  });
+});
+
+async function findOrCreateDocument(documentId: string, userId: string, date: string) {
+  try {
+    const database = await connectToDatabase();
+    const notes = database.collection('notes');
+    let note = await notes.findOne({ documentId: documentId })
+    if (note) {
+        return note;
+    } else {
+       note = await notes.findOneAndUpdate(
+        { documentId: documentId, userId: userId, date: date },
+        { $setOnInsert: { data: {}, lastUpdated: new Date() } },
+        { upsert: true, returnOriginal: false }
+      );
+      return note;
+    }
+  } catch {
+    await client.close();
+    console.log("error");
+  }
+}
+
+async function findDocument(documentId: string){
+  try {
+    const database = await connectToDatabase();
+    const notes = database.collection('notes');
+    const note = await notes.findOne({ documentId });
+    return note;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+async function saveDocument(documentId: string, data: any){
+    const database = await connectToDatabase();
+    const notes = database.collection('notes');
+    await notes.findOneAndUpdate({ documentId: documentId }, { $set: { data, lastUpdated: new Date() }})
+}
+
+async function deleteDocument(documentId: string){
+  try {
+    const database = await connectToDatabase();
+    const notes = database.collection('notes');
+    const note = await notes.findOne({ documentId: documentId });
+      if (note) {
+        await notes.deleteOne({ documentId: documentId });
+      }
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+exports.socket = functions.https.onRequest(app);
