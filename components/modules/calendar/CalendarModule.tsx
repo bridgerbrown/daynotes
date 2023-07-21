3448319fea013e3ef52cc4b360d2de67c92a77f2
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-07-21 14:27:29 -0700

    that fixed the cal render error, cleaned up the code

--- components/modules/calendar/CalendarModule.tsx
+++ components/modules/calendar/CalendarModule.tsx
@@ -58,111 +58,111 @@ export default function CalendarModule(props: any) {
   return (
     <div>
     {
-        usersNotes ?
-    <div className={activeCalendarCSS}>
-      <section className="w-full rounded-md">
-        <div className="w-full flex justify-center items-center bg-gray-100/80 shadow-inner"> 
-          <section className="pt-1 pb-7">
-            <div className="pl-3 pt-6 flex items-center">
-              <h2 className="flex-auto text-2xl font-light text-blackHeading">
-                {format(currentMonth, 'MMMM yyyy')}
-              </h2>
-              <button
-                type="button"
-                onClick={previousMonth}
-                className="flex items-center justify-center px-1.5 text-gray-700 hover:text-gray-500"
-              >
-                <span className="sr-only">Previous month</span>
-                <Image
-                  src={"/angle-left.png"}
-                  width={210}
-                  height={369}
-                  alt="Arrow"
-                  className='h-3 w-2 opacity-60 hover:opacity-80 transition-opacity'
-                />
-              </button>
-              <button
-                onClick={nextMonth}
-                type="button"
-                className="ml-2 flex flex-none items-center justify-center px-1.5 text-gray-700 hover:text-gray-500"
-              >
-                <span className="sr-only">Next month</span>
-                <Image
-                  src={"/angle-left.png"}
-                  width={210}
-                  height={369}
-                  alt="Arrow"
-                  className='h-3 w-2 opacity-60 hover:opacity-80 transition-opacity rotate-180'
-                />
-              </button>
-            </div>
-            <div className="tracking-wider text-gray-900 grid grid-cols-7 mt-4 text-sm font-medium leading-4 text-center">
-              <div>SUN</div>
-              <div>MON</div>
-              <div>TUE</div>
-              <div>WED</div>
-              <div>THU</div>
-              <div>FRI</div>
-              <div>SAT</div>
-            </div>
-            <div className="font-light border-[1px] drop-shadow-sm bg-[#f9fbfa] border-gray-300 grid grid-cols-7 my-2 text-md">
-              {days.map((day, dayIdx) => (
-                <div
-                  key={day.toString()}
-                  className={classNames(
-                    dayIdx === 0 && colStartClasses[getDay(day)],
-                    ''
-                  )}
-                >
+      usersNotes ?
+        <div className={activeCalendarCSS}>
+          <section className="w-full rounded-md">
+            <div className="w-full flex justify-center items-center bg-gray-100/80 shadow-inner"> 
+              <section className="pt-1 pb-7">
+                <div className="pl-3 pt-6 flex items-center">
+                  <h2 className="flex-auto text-2xl font-light text-blackHeading">
+                    {format(currentMonth, 'MMMM yyyy')}
+                  </h2>
                   <button
                     type="button"
-                    onClick={() => { 
-                      setSelectedDay(day)
-                      router.push(`/${usersEmail}/${day}`)
-                    }}
-                    className={classNames(
-                      isEqual(day, selectedDay) && 'bg-blue-100',
-                      !isEqual(day, selectedDay) &&
-                        isToday(day) &&
-                        'bg-blue-200 font-semibold',
-                      !isEqual(day, selectedDay) &&
-                        !isToday(day) &&
-                        isSameMonth(day, firstDayCurrentMonth) &&
-                        'text-gray-700',
-                      !isEqual(day, selectedDay) &&
-                        !isToday(day) &&
-                        !isSameMonth(day, firstDayCurrentMonth) &&
-                        'text-gray-400',
-                      isEqual(day, selectedDay) && isToday(day) && 'font-semibold bg-blue-300',
-                      isEqual(day, selectedDay) &&
-                        !isToday(day) &&
-                        'bg-blue-300',
-                      !isEqual(day, selectedDay) && 'hover:bg-gray-100 transition-colors',
-                      (isEqual(day, selectedDay) && isToday(day)) &&
-                        'bg-blue-200 font-semibold',
-                      'font-regular text-sm flex pb-5 h-14 w-14 border-[0.5px] border-gray-300 items-center justify-center'
-                    )}
+                    onClick={previousMonth}
+                    className="flex items-center justify-center px-1.5 text-gray-700 hover:text-gray-500"
                   >
-                    <time dateTime={format(day, 'yyyy-MM-dd')}>
-                      {format(day, 'd')}
-                    </time>
+                    <span className="sr-only">Previous month</span>
+                    <Image
+                      src={"/angle-left.png"}
+                      width={210}
+                      height={369}
+                      alt="Arrow"
+                      className='h-3 w-2 opacity-60 hover:opacity-80 transition-opacity'
+                    />
                   </button>
-
-                  <div className="relative bottom-5 h-0 w-14 flex justify-center">
-                    {
-                      userNotesDates.map((date: any) => isSameDay(parseISO(date), day) ? <div className="relative w-2 h-2 rounded-full bg-blue-500"></div> : <div></div>)
-                     }
-                  </div>
-
+                  <button
+                    onClick={nextMonth}
+                    type="button"
+                    className="ml-2 flex flex-none items-center justify-center px-1.5 text-gray-700 hover:text-gray-500"
+                  >
+                    <span className="sr-only">Next month</span>
+                    <Image
+                      src={"/angle-left.png"}
+                      width={210}
+                      height={369}
+                      alt="Arrow"
+                      className='h-3 w-2 opacity-60 hover:opacity-80 transition-opacity rotate-180'
+                    />
+                  </button>
                 </div>
-              ))}
+                <div className="tracking-wider text-gray-900 grid grid-cols-7 mt-4 text-sm font-medium leading-4 text-center">
+                  <div>SUN</div>
+                  <div>MON</div>
+                  <div>TUE</div>
+                  <div>WED</div>
+                  <div>THU</div>
+                  <div>FRI</div>
+                  <div>SAT</div>
+                </div>
+                <div className="font-light border-[1px] drop-shadow-sm bg-[#f9fbfa] border-gray-300 grid grid-cols-7 my-2 text-md">
+                  {days.map((day, dayIdx) => (
+                    <div
+                      key={day.toString()}
+                      className={classNames(
+                        dayIdx === 0 && colStartClasses[getDay(day)],
+                        ''
+                      )}
+                    >
+                      <button
+                        type="button"
+                        onClick={() => { 
+                          setSelectedDay(day)
+                          router.push(`/${usersEmail}/${day}`)
+                        }}
+                        className={classNames(
+                          isEqual(day, selectedDay) && 'bg-blue-100',
+                          !isEqual(day, selectedDay) &&
+                            isToday(day) &&
+                            'bg-blue-200 font-semibold',
+                          !isEqual(day, selectedDay) &&
+                            !isToday(day) &&
+                            isSameMonth(day, firstDayCurrentMonth) &&
+                            'text-gray-700',
+                          !isEqual(day, selectedDay) &&
+                            !isToday(day) &&
+                            !isSameMonth(day, firstDayCurrentMonth) &&
+                            'text-gray-400',
+                          isEqual(day, selectedDay) && isToday(day) && 'font-semibold bg-blue-300',
+                          isEqual(day, selectedDay) &&
+                            !isToday(day) &&
+                            'bg-blue-300',
+                          !isEqual(day, selectedDay) && 'hover:bg-gray-100 transition-colors',
+                          (isEqual(day, selectedDay) && isToday(day)) &&
+                            'bg-blue-200 font-semibold',
+                          'font-regular text-sm flex pb-5 h-14 w-14 border-[0.5px] border-gray-300 items-center justify-center'
+                        )}
+                      >
+                        <time dateTime={format(day, 'yyyy-MM-dd')}>
+                          {format(day, 'd')}
+                        </time>
+                      </button>
+
+                      <div className="relative bottom-5 h-0 w-14 flex justify-center">
+                        {
+                          userNotesDates.map((date: any) => isSameDay(parseISO(date), day) ? <div className="relative w-2 h-2 rounded-full bg-blue-500"></div> : <div></div>)
+                         }
+                      </div>
+
+                    </div>
+                  ))}
+                </div>
+              </section>
             </div>
           </section>
         </div>
-      </section>
-    </div>
-    :
-    <div></div>
+        :
+        <div></div>
       }
     </div>
   )
