1e6d7aaf5f356f87e37d03b05d64e203372516db
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-07-30 23:22:57 -0700

    or fix

--- pages/[user]/[date].tsx
+++ pages/[user]/[date].tsx
@@ -169,7 +169,7 @@ export default function DayNote({userCtxt}: InferGetServerSidePropsType<typeof g
   }, [selectedDay])
 
   useEffect(() => {
-    if (!usersNotes.length) return;
+    if (!usersNotes || !usersNotes.length) return;
     const noteExists = checkNoteExists(usersNotes, selectedDay);
     setNoteActivated(noteExists);
   }, [usersNotes, selectedDay])
