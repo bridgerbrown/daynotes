44e392e1fa6954a8f2221e20807824f3cc83f17c
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-10-23 14:29:27 -0700

    parse isogi

--- frontend/pages/[user]/index.tsx
+++ frontend/pages/[user]/index.tsx
@@ -136,7 +136,7 @@ export default function User({ userResponse }: InferGetServerSidePropsType<typeo
                           </div>
                           <div className='flex flex-col items-center mt-2 text-sm'>
                             <p>
-                              Member since {format(userData.memberSince, 'M/dd/yyyy')}
+                              Member since {format(parseISO(userData.memberSince), 'M/dd/yyyy')}
                             </p>
                           </div>
                         </div>
